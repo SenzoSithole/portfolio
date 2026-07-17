3dc5bb3fffaf800d0b92de295a68a5f3bbbef759
Senzo <senzosithole656@gmail.com>
2026-07-17 17:31:12 +0200

    feat: enhance LoadingScreen component with text animation and loading completion callback

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
+import { useState } from "react";
 import "./App.css";
 import { LoadingScreen } from "./components/LoadingScreen";
+import "./index.css";
 
 function App() {
-  return (
-    <>
-      <LoadingScreen />
-    </>
-  );
+  const [isLoaded, setIsLoaded] = useState(false);
+  return <>{!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}</>;
 }
 
 export default App;
