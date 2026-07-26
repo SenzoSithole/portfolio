11f6572aef58563fc68fd8e8ad2cc898cead58bf
Senzo <senzosithole656@gmail.com>
2026-07-26 20:02:41 +0200

    feat: implement Navbar and MobileMenu components; enhance LoadingScreen with animation

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export const LoadingScreen = (onComplete) => {
+export const LoadingScreen = ({ onComplete }) => {
   const [text, setText] = useState("");
   const fullText = "<Hello World! />";
 
@@ -30,11 +30,11 @@ export const LoadingScreen = (onComplete) => {
 
       {/* Loading bar animation code does not work as expected , need to debug */}
 
-      {/* <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden"> */}
-      {/* <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"> */}
-      {""}
+      <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
+        <div className="w-[40%] h-full bg-green-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar">
+          {""}
+        </div>
+      </div>
     </div>
-    // </div>
-    // </div>
   );
 };
